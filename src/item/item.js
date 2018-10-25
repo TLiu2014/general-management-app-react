0bb6b1aebb5ffcf30b299ec44b20b57bf9685317
Tianwei Liu <TianweiLiu2012@gmail.com>
2018-10-25 15:01:45 -0400

    Added item & item table components

---
+++ src/item/item.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './item.scss';
+
+class Item extends Component {
+  render() {
+    return (
+        <div></div>
+    );
+  }
+}
+
+export default Item;
