2c6c4924b0d69b8426fca908b991081122a79570
Tianwei Liu <TianweiLiu2012@gmail.com>
2018-10-30 14:31:42 -0400

    Added POST API integration.
    Will display new added item with the POST response data

--- src/itemTable/itemTable.js
+++ src/itemTable/itemTable.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from 'reactstrap';
+import { Table, Button, Input } from 'reactstrap';
 import { Environment } from '../config';
 import './itemTable.scss';
 
@@ -9,28 +9,73 @@ class ItemTable extends Component {
     this.state = {
       error: null,
       isLoaded: false,
-      items: []
+      items: [],
+      newItemName: "",
+      newItemValue: "",
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch(Environment.API_URL + "/items", {
+      method: 'POST',
+      headers: Environment.HTTP_OPTIONS.headers,
+      body: JSON.stringify({
+        name: this.state.newItemName,
+        value: this.state.newItemValue,
+      })
+    })
+    .then(res => res.json())
+    .then(
+      (result) => {
+        console.log('POST item response', result);
+        this.setState({
+          newItemName: "",
+          newItemValue: "",
+          items: [...this.state.items, result]
+        });
+      },
+      (error) => {
+        this.setState({
+          // isLoaded: true,
+          error
+        });
+      }
+    );
+  }
+
+
+
   componentDidMount() {
-    fetch(Environment.API_URL + "/item/1", Environment.HTTP_OPTIONS)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          console.log(result);
-          this.setState({
-            isLoaded: true,
-            items: result
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+    fetch(Environment.API_URL + "/items", {
+      method: 'GET',
+      headers: Environment.HTTP_OPTIONS.headers
+    })
+    .then(res => res.json())
+    .then(
+      (result) => {
+        console.log('GET all items response', result);
+        this.setState({
+          isLoaded: true,
+          items: result
+        });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    );
   }
 
   render() {
@@ -41,9 +86,9 @@ class ItemTable extends Component {
       return <div>Loading...</div>;
     } else {
       const itemTableView = items.map((item, index) =>
-        <tr key={item.item_id}>
-          <th scope="row">{index}</th>
-          <th>{item.item_id}</th>
+        <tr key={item.itemId}>
+          <th scope="row">{index + 1}</th>
+          <th>{item.itemId}</th>
           <td>{item.name}</td>
           <td>{item.value}</td>
         </tr>
@@ -51,19 +96,24 @@ class ItemTable extends Component {
       return (
         <div>
           <Table striped bordered hover responsive>
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">ID</th>
-              <th scope="col">Name</th>
-              <th scope="col">Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            {itemTableView}
-          </tbody>
-        </Table>
-      </div>
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              {itemTableView}
+            </tbody>
+          </Table>
+          <form onSubmit={this.handleSubmit}>
+            <Input placeholder="Name" name="newItemName" value={this.state.newItemName} onChange={this.handleChange}/>
+            <Input placeholder="Value" name="newItemValue" value={this.state.newItemValue} onChange={this.handleChange}/>
+            <Button type="submit" color="primary">Submit</Button>
+          </form>
+        </div>
       );
     }
   }
